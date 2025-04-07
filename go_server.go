168e70d552bd505236b72034295eafdb163321f5
code-bugg <ilielungu332@gmail.com>
2025-04-07 14:52:24 +0300

    Removed C and C++ servers

--- go_server.go
+++ go_server.go
@@ -1,15 +1,15 @@
-package main
+package server
 
 import (
 	"fmt"
-	"net"
 	"io"
+	"net"
 )
 
 func handleClient(conn net.Conn) {
 	defer conn.Close()
 	buffer := make([]byte, 1024)
-	
+
 	for {
 		n, err := conn.Read(buffer)
 		if err != nil {
@@ -22,19 +22,19 @@ func handleClient(conn net.Conn) {
 	}
 }
 
-func main(){
+func main() {
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 	defer listener.Close()
-	
+
 	fmt.Println("Go Server listening on port 8080...")
-	
-	for{
+
+	for {
 		conn, err := listener.Accept()
-		if err != nil{
+		if err != nil {
 			fmt.Println("Connection error: ", err)
 			continue
 		}
